8002c70d49e4996b6ed9aba56fb3ff68184e4877
lydiaket <lydiaketema@gmail.com>
2025-07-17 23:12:07 -0400

    amplify config fix

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "elesite": {
-      "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string",
-      "GraphQLAPIKeyOutput": "string"
-    }
-  }
-}
